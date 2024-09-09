077eafa690779bd1537ed3266029300ec00f5c23
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-09-10 00:39:30 +0900

    update: クッキーパラメータを設定

--- middleware/global/SessionsSetupMiddleware.php
+++ middleware/global/SessionsSetupMiddleware.php
@@ -9,6 +9,11 @@ class SessionsSetupMiddleware implements Middleware {
     public function handle(Callable $next): HTTPRenderer {
         error_log("Setting up sessions...");
 
+        session_set_cookie_params([
+            "lifetime" => 0,
+            "httponly" => true,
+            "samesite" => "Lax",
+        ]);
         session_start();
 
         return $next();
