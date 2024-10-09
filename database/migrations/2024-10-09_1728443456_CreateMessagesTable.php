db7a5a5c89a71ca242c154ea9598c9e990866da0
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-10-09 18:44:49 +0900

    db-migrate: messagesテーブルを作成

---
+++ database/migrations/2024-10-09_1728443456_CreateMessagesTable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateMessagesTable implements SchemaMigration {
+    public function up(): array {
+        // TODO: マイグレーションロジックを追加
+        return [
+            "CREATE TABLE messages (
+                message_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                from_user_id INT UNSIGNED NOT NULL,
+                to_user_id INT UNSIGNED NOT NULL,
+                content VARCHAR(255) NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (from_user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+                FOREIGN KEY (to_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+            )"
+        ];
+    }
+
+    public function down(): array {
+        // TODO: ロールバックロジックを追加
+        return [
+            "DROP TABLE messages"
+        ];
+    }
+}
