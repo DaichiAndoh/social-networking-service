3e0ae7eb82aead76345f6fa07f9dce96a80a9989
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-10-18 09:26:23 +0900

    update: トップ画面を修正

--- views/page/top.php
+++ views/page/top.php
@@ -1,6 +1,38 @@
-<div class="container-fluid py-4 fixed-bottom" style="background-color: #051c2c;">
-    <div class="text-center">
-        <a href="/login" class="btn btn-primary">ログイン</a>
-        <a href="/register" class="btn btn-light ms-4">ユーザー登録</a>
+<div class="d-flex justify-content-center" style="height: 100vh;">
+    <div style="margin-top: 10vh; width: 350px;">
+        <div class="mb-4">
+            <h3 class="typing">すべての話題が、ここに。</h3>
+            <h5>今すぐ参加しよう。</h5>
+        </div>
+        <div class="card py-2">
+            <div class="card-body">
+                <div class="mb-3">
+                    <a href="/login" class="btn btn-primary w-100">ログイン</a>
+                </div>
+                <div>
+                    <a href="/register" class="btn btn-light w-100">ユーザー登録</a>
+                </div>
+                <div class="login-label d-flex justify-content-center align-items-center m-0 my-3">
+                    <small>または</small>
+                </div>
+                <div>
+                    <div class="mb-3">
+                        <a href="/login" class="btn btn-outline-primary w-100">ゲストログイン</a>
+                    </div>
+                    <div class="alert alert-danger d-flex gap-1 py-2 px-1 m-0" role="alert">
+                        <div>
+                            <ion-icon name="alert-circle-outline" style="font-size: 26px;"></ion-icon>
+                        </div>
+                        <div>
+                            <small>
+                                ゲストログインで使用するアカウントは、ゲストログインを行う全てのユーザーと共通になります。
+                                そのため、ログイン後の操作では個人情報や機密情報等を扱わないようにしてください。<br>
+                                また、ゲストユーザーのアカウントに紐づくデータは、予告せず管理者側で削除等の操作を行う可能性があります。
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
